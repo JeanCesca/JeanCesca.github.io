0de5cbb30baca22753bbef8ae6c3757c78797c14
Jean Cesca <jeanrcesca@gmail.com>
2022-05-20 11:43:15 -0300

    dropdown menu refatorado

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,11 +1,15 @@
-export default class AnimacaoScroll {
+import debounce from './debounce.js';
+
+export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
+  // Pega a distância de cada item em relação
+  // ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
@@ -16,12 +20,14 @@ export default class AnimacaoScroll {
     });
   }
 
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
-        item.element.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        item.element.classList.remove("ativo");
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
@@ -30,12 +36,13 @@ export default class AnimacaoScroll {
     if (this.sections.length) {
       this.getDistance();
       this.checkDistance();
-      window.addEventListener("scroll", this.checkDistance);
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
   }
-  
+
+  // Remove o event de scroll
   stop() {
-    window.removeEventListener("scroll", this.checkDistance);
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
